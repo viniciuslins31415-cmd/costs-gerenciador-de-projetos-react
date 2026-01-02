bd8b085e832b5567c2f40fe92d23139cdbf2bf0d
Vinicius Lins Rolim <viniciuslins31415@gmail.com>
2026-01-02 10:00:22 -0300

    v1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Projects from "./components/pages/Projects"
+
+import Container from "./components/layout/Container"
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
+
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
