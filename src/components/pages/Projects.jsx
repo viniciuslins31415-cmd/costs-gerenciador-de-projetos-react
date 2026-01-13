e04b98e0b3b30ac0e84bbd6aa1160c218ae19b3b
Vinicius Lins Rolim <viniciuslins31415@gmail.com>
2026-01-13 14:14:11 -0300

    message component

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,19 @@
+import { useLocation } from "react-router-dom"
+
+import Message from "../layout/Message"
+
 function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
     return(
         <div>
-            <h1>Projects</h1>
+            <h1>Meus projetos</h1>
+            {message && <Message type="success" msg={message}/>}
         </div>
     )
 }
